52897429b8e0b8167ee0dad28f402abca86b72af
namratayevale2602 <namratayevale7@gmail.com>
2025-12-10 13:36:49 +0530

    ui

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8000/api";
-export const IMAGE_PATH = "http://localhost:8000/uploads";
+export const API_BASE_URL = "https://masma-back.demovoting.com/api";
+export const IMAGE_PATH = "https://masma-back.demovoting.com/uploads";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
